14c86eea7118cc12459d0b96d2a1490b5e65d564
Mfundo Mgubo <mfundomgubo@capitecbank.co.za>
2025-12-11 17:48:11 +0200

    creating footer component

--- app_mobile-frontend_capisto/src/App.jsx
+++ app_mobile-frontend_capisto/src/App.jsx
@@ -9,6 +9,7 @@ import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -24,6 +25,7 @@ const App = () => {
         <Route path='/my-appointments' element={<MyAppointments />} />
         <Route path='/appointments' element={<Appointment />} />
       </Routes>
+      <Footer />
        
     </div>
   )
